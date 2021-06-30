628a572fe084ab07ea5c1e05edb447ebc2c86913
Thays alves santana lima <85905916+Thays121@users.noreply.github.com>
2021-06-30 16:21:22 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,30 +5,31 @@ print('* Multiplicação')
 print('/ Divisão')
 print('Pressione outra tecla para sair!')
 
-op = input('Qual operação deseja fazer?')
-if op == '+' or op == '-' or op == '*' or op == '/':
-    x = int(input('Digite o primeiro valor:'))
-    y =int(input('Digite o segundo valor:'))
-
-while (op != 's'): 
-    if (op == '+'):
-        res = x + y
-        print('Resultado: {} + {} = {}'.format(x, y, res))
-    elif(op == '-'):
-        res = x - y
-        print('Resultado: {} - {} = {}'.format(x, y, res))
-    elif (op == '*'):
-        res = x * y
-        print('Resultado: {} * {} = {}'.format(x, y, res))
-    elif (op == '/'):
-        res = x / y
-        print('Resultado: {} / {} = {}'.format(x, y, res))
-    else:
-        print('Operação invalida.')
-
-    op = input('Qual operação deseja fazer?')
-    if op == '+' or op == '-' or op == '*' or op == '/':
-        x = int(input('Digite o primeiro valor:'))
-        y =int(input('Digite o segundo valor:'))
+while True: 
+      op = input('Qual operação deseja fazer?')
+      if op == '+' or op == '-' or op == '*' or op == '/':
+          x = int(input('Digite o primeiro valor:'))
+          y = int(input('Digite o segundo valor:'))
+          
+      if (op == '+'):
+          res = x + y
+          print('Resultado: {} + {} = {}'.format(x, y, res))
+          continue
+      elif(op == '-'):
+          res = x - y
+          print('Resultado: {} - {} = {}'.format(x, y, res))
+          continue
+      elif (op == '*'):
+          res = x * y
+          print('Resultado: {} * {} = {}'.format(x, y, res))
+          continue
+      elif (op == '/'):
+          res = x / y
+          print('Resultado: {} / {} = {}'.format(x, y, res))
+          continue
+      elif (op == 's'):
+          break
+      else:
+          print('Operação invalida.')
 
 print('Encerrando o programa...')
