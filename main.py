04d6fb9d694100db9870476f2966ab537db8c798
Thays alves santana lima <85905916+Thays121@users.noreply.github.com>
2021-06-30 15:36:00 -0400

    Update main.py

--- main.py
+++ main.py
@@ -10,19 +10,25 @@ if op == '+' or op == '-' or op == '*' or op == '/':
     x = int(input('Digite o primeiro valor:'))
     y =int(input('Digite o segundo valor:'))
 
-if (op == '+'):
-    res = x + y
-    print('Resultado: {} + {} = {}'.format(x, y, res))
-elif(op == '-'):
-    res = x - y
-    print('Resultado: {} - {} = {}'.format(x, y, res))
-elif (op == '*'):
-    res = x * y
-    print('Resultado: {} * {} = {}'.format(x, y, res))
-elif (op == '/'):
-    res = x / y
-    print('Resultado: {} / {} = {}'.format(x, y, res))
-else:
+while (op != 's'): 
+    if (op == '+'):
+        res = x + y
+        print('Resultado: {} + {} = {}'.format(x, y, res))
+    elif(op == '-'):
+        res = x - y
+        print('Resultado: {} - {} = {}'.format(x, y, res))
+    elif (op == '*'):
+        res = x * y
+        print('Resultado: {} * {} = {}'.format(x, y, res))
+    elif (op == '/'):
+        res = x / y
+        print('Resultado: {} / {} = {}'.format(x, y, res))
+    else:
     print('Operação invalida.')
+
+    op = input('Qual operação deseja fazer?')
+    if op == '+' or op == '-' or op == '*' or op == '/':
+        x = int(input('Digite o primeiro valor:'))
+        y =int(input('Digite o segundo valor:'))
 
 print('Encerrando o programa...')
